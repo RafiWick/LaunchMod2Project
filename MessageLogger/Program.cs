719744c56af0b084eaee12ec6b966e984ace8c9d
RafiWick <rafiwick@gmail.com>
2023-06-12 11:07:44 -0600

    user experience updated and user finding/creating put into methods

--- MessageLogger/Program.cs
+++ MessageLogger/Program.cs
@@ -5,32 +5,23 @@ using Microsoft.EntityFrameworkCore;
 
 using (var context = new MessageLoggerContext())
 {
-    Console.WriteLine("Welcome to Message Logger!");
-    Console.WriteLine();
-    Console.WriteLine("Let's create a user pofile for you.");
-    Console.Write("What is your name? ");
-    string name = Console.ReadLine();
-    Console.Write("What is your username? (one word, no spaces!) ");
-    string username = Console.ReadLine();
-    User user = new User(name, username);
-    
-    context.Users.Add(user);
-    context.SaveChanges();
-
-    Console.WriteLine();
-    Console.WriteLine("To log out of your user profile, enter `log out`.");
-
-    Console.WriteLine();
-    Console.Write("Add a message (or `quit` to exit): ");
-
-    string userInput = Console.ReadLine();
-    //List<User> users = new List<User>() { user };
+    User user = null;
+    string userInput = "";
+    if (!context.Users.Any())
+    {
+        user = NewUser(context);
+        userInput = Console.ReadLine();
+    }
+    else
+    {
+        userInput = "log out";
+    }
 
     while (userInput.ToLower() != "quit")
     {
         while (userInput.ToLower() != "log out")
         {
-            
+
             user.Messages.Add(new Message(userInput));
             context.SaveChanges();
 
@@ -49,29 +40,14 @@ using (var context = new MessageLoggerContext())
         userInput = Console.ReadLine();
         if (userInput.ToLower() == "new")
         {
-            Console.Write("What is your name? ");
-            name = Console.ReadLine();
-            Console.Write("What is your username? (one word, no spaces!) ");
-            username = Console.ReadLine();
-            user = new User(name, username);
-            context.Users.Add(user);
-            Console.Write("Add a message: ");
+            user = NewUser(context);
 
             userInput = Console.ReadLine();
 
         }
         else if (userInput.ToLower() == "existing")
         {
-            Console.Write("What is your username? ");
-            username = Console.ReadLine();
-            user = null;
-            foreach (var existingUser in context.Users)
-            {
-                if (existingUser.Username == username)
-                {
-                    user = existingUser;
-                }
-            }
+            user = ExistingUser(context);
 
             if (user != null)
             {
@@ -93,4 +69,41 @@ using (var context = new MessageLoggerContext())
     {
         Console.WriteLine($"{u.Name} wrote {u.Messages.Count} messages.");
     }
+}
+
+User NewUser(MessageLoggerContext context)
+{
+
+    Console.WriteLine("Welcome to Message Logger!");
+    Console.WriteLine();
+    Console.WriteLine("Let's create a user pofile for you.");
+    Console.Write("What is your name? ");
+    string name = Console.ReadLine();
+    Console.Write("What is your username? (one word, no spaces!) ");
+    string username = Console.ReadLine();
+    User user = new User(name, username);
+
+    context.Users.Add(user);
+    context.SaveChanges();
+
+    Console.WriteLine();
+    Console.WriteLine("To log out of your user profile, enter `log out`.");
+
+    Console.WriteLine();
+    Console.Write("Add a message (or `quit` to exit): ");
+    return user;
+}
+User ExistingUser(MessageLoggerContext context)
+{
+    Console.Write("What is your username? ");
+    string username = Console.ReadLine();
+    User user = null;
+    foreach (var existingUser in context.Users)
+    {
+        if (existingUser.Username == username)
+        {
+            user = existingUser;
+        }
+    }
+    return user;
 }
